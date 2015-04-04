f5e7725f6d1c01e9af13702e2ff3c3b28b499b3e
ahmad121 <ahmadforoozani@gmail.com>
2015-04-04 12:04:30 +0000

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },   uniqueness: { case_sensitive: false }
-
 has_secure_password
+  
+validates :password, length: { minimum: 6 }, allow_blank: true
 
-validates :password, length: {minimum:6}
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150314145635) do
+ActiveRecord::Schema.define(version: 20150404115613) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "gender"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
